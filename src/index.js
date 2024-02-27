6b98c06626b3074ae3222b292b5cd7452c5b08f0
brrsmths <matheusbsouza43@gmail.com>
2024-02-26 23:35:06 -0300

    add comments

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
+// declarações
 const precoCombustivel = 5.79;
 const gastoMedio = 10;
 let distanciaViagemEmKM = 938;
 
+//processo lógico
 let litrosConsumidos = distanciaViagemEmKM / gastoMedio;
 let valorGasto = litrosConsumidos * precoCombustivel;
+
+//saída
 console.log(`O valor gasto em combustivel nesta viagem de ${distanciaViagemEmKM} KM, foi de R$${valorGasto}.`)
 
 
