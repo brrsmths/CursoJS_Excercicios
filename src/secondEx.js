c7f0f327452ac8f06e9e13e6313b771911fdbb1a
brrsmths <matheusbsouza43@gmail.com>
2024-02-28 23:02:02 -0300

    changes in variables

--- src/secondEx.js
+++ src/secondEx.js
@@ -1,18 +1,18 @@
 let tipoCombustivel = "Etanol"
+let precoGasolina = 5.77
+let precoEtanol = 3.88
 let gastoMedioPorKM = 10
 let distanciaViagem = 100
 let valorGasto
 
 //processo lógico
 if (tipoCombustivel === "Gasolina"){
-    let precoCombustivel = 5.77
     let litrosConsumidos = distanciaViagem / gastoMedioPorKM
-    let valorGasto = litrosConsumidos * precoCombustivel
+    let valorGasto = litrosConsumidos * precoGasolina
     console.log(`O valor gasto nessa viagem foi de R$${valorGasto.toFixed(2)}`)
 } else if(tipoCombustivel === "Etanol"){
-    let precoCombustivel = 3.88
     let litrosConsumidos = distanciaViagem / gastoMedioPorKM
-    let valorGasto = litrosConsumidos * precoCombustivel
+    let valorGasto = litrosConsumidos * precoEtanol
     console.log(`O valor gasto nessa viagem foi de R$${valorGasto.toFixed(2)}`)
 } else {
     console.log("Não foi informado o tipo de combustivel")
